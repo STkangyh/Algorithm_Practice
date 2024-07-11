6d89f280d29bf5036f9b9163e4d1f89645aae74e
STkangyh <k22101149@g.seoultech.ac.kr>
2024-07-11 10:20:59 +0900

    2805 이분탐색

---
+++ Baekjun/Silver/2805.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+trees = list(map(int, input().split()))
+start, end = 1, max(trees)
+
+while start <= end:
+    mid = (start + end)//2
+    log = 0
+    for i in trees:
+        if i >= mid:
+            log += i - mid
+    
+    if log >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+    print(f'{start} {mid} {end}')
+
+print(end)
