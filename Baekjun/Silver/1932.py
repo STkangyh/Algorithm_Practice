48db48a751b4877de7d106a55ddb18a7788817ac
STkangyh <k22101149@g.seoultech.ac.kr>
2024-07-16 10:41:56 +0900

    1932 dp 점화식 수식으로 쓰면서 풀어보기

---
+++ Baekjun/Silver/1932.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for i in range(n):
+    arr.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(len(arr[i])):
+        if j == 0:
+            arr[i][j] = arr[i][j] + arr[i-1][j]
+        elif j == len(arr[i]) - 1:
+            arr[i][j] = arr[i][j] + arr[i-1][j-1]
+        else:
+            arr[i][j] = arr[i][j] + max(arr[i-1][j-1], arr[i-1][j])
+print(max(arr[-1]))
